b619b0b0931c0ed7e96d8a6dc9403f073a66dbf7
hyucksu.jang <hyucksu.jang@samsung.com>
2018-02-17 15:02:19 +0900

    code 1차 커밋

--- pizzaops/src/main/java/com/pizza/common/code/controller/CodeController.java
+++ pizzaops/src/main/java/com/pizza/common/code/controller/CodeController.java
@@ -33,27 +33,37 @@ public class CodeController {
 	
     @RequestMapping("/moveCodeList.do")
     private ModelAndView moveUserList() throws Exception {
-    	ModelAndView mv = new ModelAndView("code/codeList");
+    	ModelAndView mv = new ModelAndView("code/classCodeList");
     	return mv;
     }
     
-    @RequestMapping(value = "/codeMgmt/selectListClassCode.do", method=RequestMethod.POST)
-    private ModelAndView selectListClassCode(@ModelAttribute ClassCodeVO classCodeVO, HttpServletRequest request) throws Exception {
+    @RequestMapping(value = "/codeMgmt/selectOneClassCode.do", method=RequestMethod.POST)
+    private ModelAndView selectListClassCode(@RequestBody ClassCodeVO classCodeVO, HttpServletRequest request) throws Exception {
 		
 		ModelAndView mv = new ModelAndView("jsonView");
 		
-		List<ClassCodeVO> codeList = codeSvc.selectListClassCode(classCodeVO);
+		ClassCodeVO classCodeInfo = codeSvc.selectOneClassCode(classCodeVO);
 		
-		mv.addObject("data", codeList);
+		mv.addObject("classCodeInfo", classCodeInfo);
 		
 		return mv;
     }
     
+    @RequestMapping(value = "/codeMgmt/selectListClassCode.do", method=RequestMethod.POST)
+    private ModelAndView selectOneClassCode(@ModelAttribute ClassCodeVO classCodeVO, HttpServletRequest request) throws Exception {
+    	
+    	ModelAndView mv = new ModelAndView("jsonView");
+    	
+    	
+    	List<ClassCodeVO> codeList = codeSvc.selectListClassCode(classCodeVO);
+    	
+    	mv.addObject("data", codeList);
+    	
+    	return mv;
+    }
+    
     @RequestMapping(value = "/codeMgmt/addClassCode.do", method=RequestMethod.POST)
     private RestResponseVO addClassCode(@RequestBody ClassCodeVO classCodeVO, HttpServletRequest request) throws Exception {
-    	
-    	System.out.println(classCodeVO.getClassCodeName());
-    	System.out.println(classCodeVO.getDescription());
     	
     	RestResponseVO res = new RestResponseVO(200);
     	
@@ -77,6 +87,35 @@ public class CodeController {
     	 * 3. insert svc 호출
     	 * */
 		codeSvc.insertClassCode(classCodeVO);
+    	
+    	return res;
+    }
+    
+    @RequestMapping(value = "/codeMgmt/deleteClassCode.do", method=RequestMethod.POST)
+    private RestResponseVO deleteClassCode(@RequestBody ClassCodeVO classCodeVO) throws Exception {
+    	
+    	RestResponseVO res = new RestResponseVO(200);
+    	/*
+    	 * 1. delete svc 호출
+    	 * code의 경우, 살제후 동일한 이름으로 재등록 할 경우가 많기 때문에 물리적 삭제 한다.
+    	 * */
+		codeSvc.deleteClassCode(classCodeVO);
+    	
+    	return res;
+    }
+    
+    @RequestMapping(value = "/codeMgmt/updateClassCode.do", method=RequestMethod.POST)
+    private RestResponseVO updateClassCode(@RequestBody ClassCodeVO classCodeVO, HttpServletRequest request) throws Exception {
+    	
+    	RestResponseVO res = new RestResponseVO(200);
+    	
+    	/*
+    	 * 2.로그인 사용자 조회 - setFirstCreateUserId, setLastModUserId
+    	 * */
+    	UserVO loginUser = PizzaSessionUtil.getLoginUser(request);
+    	classCodeVO.setLastModUserId(loginUser.getUserId());
+    	
+    	codeSvc.updateClassCode(classCodeVO);
     	
     	return res;
     }
